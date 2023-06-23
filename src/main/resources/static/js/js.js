82da40ec90d68ad5a120efd7617c20f3b4fe4091
Niall-J-Murray <niall_murray@outlook.com>
2023-06-23 12:58:55 +0100

    Added practice draft feature

--- src/main/resources/static/js/js.js
+++
@@ -1,9 +0,0 @@
-let src = $('.background').css('background-image');
-let url = src.match(/\((.*?)\)/)[1].replace(/('|")/g,'');
-
-let img = new Image();
-img.onload = function() {
-    alert('image loaded');
-}
-img.src = url;
-// if (img.complete) img.onload();
