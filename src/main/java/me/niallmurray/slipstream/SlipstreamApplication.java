b6c0da5a5a9feb923fb79de8ce37e36c4ded02a7
Niall-J-Murray <niall_murray@outlook.com>
2023-07-24 21:41:38 +0100

    Fixed bug with new driver added

--- src/main/java/me/niallmurray/slipstream/SlipstreamApplication.java
+++ src/main/java/me/niallmurray/slipstream/SlipstreamApplication.java
@@ -1,13 +1,12 @@
 package me.niallmurray.slipstream;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SlipstreamApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(SlipstreamApplication.class, args);
-  }
+//  public static void main(String[] args) {
+//    SpringApplication.run(SlipstreamApplication.class, args);
+//  }
 
 }
